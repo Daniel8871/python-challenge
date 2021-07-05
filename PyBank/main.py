44ba7bad8ed61b7d1a8046a153fbb5c681653d6e
Dabble <85852991+Daniel8871@users.noreply.github.com>
2021-07-05 17:03:07 -0500

    Resources added to directory

---
+++ PyBank/main.py
@@ -0,0 +1,6 @@
+import os
+import csv
+
+budget_data_path=os.path.join("Resources","budget_data.csv")
+
+print(budget_data_path)
---
+++ PyPoll/main.py
@@ -0,0 +1,6 @@
+import os
+import csv
+
+# error: worng location budget_data_path=os.path.join("Resources","budget_data.csv")
+
+print(budget_data_path)
