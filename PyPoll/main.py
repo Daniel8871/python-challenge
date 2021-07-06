d76155bcb481b1fe998222c934abd9deab9ae084
Dabble <85852991+Daniel8871@users.noreply.github.com>
2021-07-06 18:38:13 -0500

    For Loops built for candidates. Attempting to build an exit

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,6 +1,29 @@
 import os
 import csv
 
-# error: worng location budget_data_path=os.path.join("Resources","budget_data.csv")
 
-print(budget_data_path)
+election_data_path=os.path.join("Resources","election_data.csv")
+
+with open(election_data_path) as fileobj:
+    election_reader=csv.reader(fileobj,delimiter=",")
+    next(election_data_path)
+
+    rowcount,cand_count=0,0
+    candidates=list()
+    current_vote=str()
+    bool= False
+
+    for row in election_reader:
+        rowcount+=1 
+        current_vote=row[2] 
+
+        for x in range(0,len(candidates)):
+            if candidates[x] == current_vote:
+                bool=True
+
+
+
+#list, then while loop, then for loop, ea loop, 
+#use for letter in word idea, if index0 matches, the 
+#turn off while loop (maybe swap whileand for loop?)
+#possible to 
