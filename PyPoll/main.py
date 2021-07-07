dda590c460aaacf46e626cdb05c28745bca529a8
Dabble <85852991+Daniel8871@users.noreply.github.com>
2021-07-06 19:16:40 -0500

    Vote tally per candidate potentially solved.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -30,6 +30,24 @@ with open(election_data_path) as fileobj:
     print(rowcount)
     print(candidates)
 
+    fileobj.seek(0)
+   
+    tally=[]
+    for y in range(0,len(candidates)):
+        tally.append(0)
+
+    for row in election_reader:
+        current_vote=row[2]
+        
+        for person in candidates:
+            if person == current_vote:
+                tally_x=candidates.index(person)
+                tally[int(tally_x)]=tally[int(tally_x)]+1
+                break
+            
+print(candidates)
+print(tally)
+
 #list, then while loop, then for loop, ea loop, 
 #use for letter in word idea, if index0 matches, the 
 #turn off while loop (maybe swap whileand for loop?)
